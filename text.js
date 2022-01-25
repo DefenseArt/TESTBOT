be6f3ebe3e0c8660f358c817bea2d64a52799281
DefenseArt <82384826+DefenseArt@users.noreply.github.com>
2022-01-26 01:30:51 +0900

    Add files via upload

---
+++ text.js
@@ -0,0 +1,28 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { DiscordTogether } = require('discord-together');
+
+client.discordTogether = new DiscordTogether(client);
+
+client.on('messageCreate', async message => { 
+    if (message.content.startsWith('!start')) {
+        if(message.member.voice.channel) {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(async invite => {
+                return message.channel.send(`${invite.code}`);
+            });
+        };
+    };
+});
+
+client.on('message', msg => {
+  if (msg.content.startsWith('!help')) {
+    msg.reply('help');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content.startsWith('!헉')){
+    msg.channel.send('헉;;');
+  }
+});
+client.login('OTMzNjE0MjQzODY4MjA1MDY3.YekGBQ.uGonfqq2CGOlCrJ_pdcyvUb_CrU');
